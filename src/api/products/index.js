851f5c5cc587dd19f6c7c7909197e11f4582b3ec
Ainafitriani <ainafitriani31@gmail.com>
2022-07-20 15:25:40 +0700

    migration orders & transactions

--- src/api/products/index.js
+++ src/api/products/index.js
@@ -4,24 +4,10 @@ const routes = require('./routes');
 module.exports = {
   name: 'products',
   version: '1.0.0',
-  register: async (server, { ProductService,storageService, validator }) => {
-    const handler = new ProductsHandler(ProductService,storageService, validator);
+  // menambahkan opsi untuk mengubah `service` menjadi `productsService`
+  // dan menambahkan `storageService`
+  register: async (server, { productsService, storageService, validator }) => {
+    const handler = new ProductsHandler(productsService, storageService, validator);
     server.route(routes(handler));
   },
 };
-
-
-
-// const ProductsHandler = require('./handler');
-// const routes = require('./routes');
-
-// module.exports = {
-//   name: 'products',
-//   version: '1.0.0',
-//   // menambahkan opsi untuk mengubah `service` menjadi `productsService`
-//   // dan menambahkan `storageService`
-//   register: async (server, { productsService, storageService, validator }) => {
-//     const handler = new ProductsHandler(productsService, storageService, validator);
-//     server.route(routes(handler));
-//   },
-// };
