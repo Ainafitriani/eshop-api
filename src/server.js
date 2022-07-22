d16e52707a0e0a2b653cf91b016129411f6b3eeb
Ainafitriani <ainafitriani31@gmail.com>
2022-07-22 19:48:36 +0700

    edit file server

--- src/server.js
+++ src/server.js
@@ -1,12 +1,11 @@
 require('dotenv').config();
-
 const Hapi = require('@hapi/hapi');
 const Jwt = require('@hapi/jwt');
-const inert = require('@hapi/inert');
-// authentication
+const Inert = require('@hapi/inert');
+
+//
 const authentication = require('./api/authentication');
 const Database = require('./conf/Database');
-const ClientError = require('./exceptions/ClientError');
 const AuthenticationService = require('./services/mysql/AuthenticationService');
 const AuthenticationValidator = require('./validator/authentication');
 
@@ -14,14 +13,32 @@ const AuthenticationValidator = require('./validator/authentication');
 const products = require('./api/products');
 const ProductsService = require('./services/mysql/ProductsService');
 const ProductsValidator = require('./validator/products');
+const ClientError = require('./exceptions/ClientError');
+
+// carts
+const carts = require('./api/carts');
+const CartsService = require('./services/mysql/CartsService');
+const CartsValidator = require('./validator/carts');
+
+// transactions
+const transactions = require('./api/transactions');
+const TransactionsService = require('./services/mysql/TransactionsService');
+
+// storage
 const StorageService = require('./services/storage/StorageService');
 const path = require('path');
+
+
 
 const init = async () => {
   const database = new Database();
   const authenticationService = new AuthenticationService(database);
   const productsService = new ProductsService(database);
-  const storageService = new StorageService(path.resolve(__dirname, '/api/products/images'));
+  const cartsService = new CartsService(database);
+  const transactionsService = new TransactionsService(database);
+  const storageService = new StorageService(path.resolve(__dirname + '/api/products/images'));
+
+
 
   const server = Hapi.server({
     host: process.env.HOST,
@@ -37,56 +54,11 @@ const init = async () => {
     method: 'GET',
     path: '/',
     handler: () => ({
-      name: 'Aina Fitriani',
+      name: 'Sany Santiastry',
     }),
   });
 
-// register external plugin
-  await server.register([
-    {
-      plugin: Jwt,
-    },
-    {
-     plugin: inert, 
-    }
-  ]);
-
-// defines authentication strategy
-  server.auth.strategy('eshop_jwt', 'jwt',{
-    keys: process.env.TOKEN_KEY,
-    verify: {
-      aud: false,
-      iss: false,
-      sub: false,
-    },
-    validate: (artifacts) => ({
-      isValid: true,
-      credentials: {
-        id: artifacts.decoded.payload.id,
-      },
-    }),
-  });
-
-  // defines internal plugins
-   await server.register([
-    {
-      plugin: authentication,
-      options: {
-        service: authenticationService,
-        validator: AuthenticationValidator,
-      },
-    },
-    {
-      plugin: products,
-      options: {
-        productsService,
-        storageService,
-        validator: ProductsValidator,
-      },
-    },
-  ]);
-
- // extension
+  // extension
   server.ext('onPreResponse', (request, h) => {
     const {response} = request;
 
@@ -103,9 +75,66 @@ const init = async () => {
 
     return h.continue;
   });
+
+  // register external plugin
+  await server.register([
+    {
+      plugin: Jwt,
+    },
+    {
+      plugin: Inert,
+    },
+  ]);
+  // defines authentication strategy
+  server.auth.strategy('eshop_jwt', 'jwt',{
+    keys: process.env.TOKEN_KEY,
+    verify: {
+      aud: false,
+      iss: false,
+      sub: false,
+    },
+    validate: (artifacts) => ({
+      isValid: true,
+      credentials: {
+        id: artifacts.decoded.payload.id,
+      },
+    }),
+  });
+  //defines internal plugin
+  await server.register([
+    {
+      plugin: authentication,
+      options: {
+        service: authenticationService,
+        validator: AuthenticationValidator,
+        
+      },
+    },
+    {
+      plugin: products,
+      options: {
+        productsService,
+        storageService,
+        validator: ProductsValidator,
+      }
+    },
+    {
+      plugin: carts,
+      options: {
+        service: cartsService,
+        validator: CartsValidator,
+      },
+    },
+    {
+      plugin: transactions,
+      options: {
+        service: transactionsService,
+      },
+    },
+  ]);
+
   await server.start();
   console.log(`Server running at ${server.info.uri}`);
-
 };
 
-init()
+init();
